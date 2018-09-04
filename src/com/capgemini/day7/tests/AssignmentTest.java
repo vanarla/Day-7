10cf905788dbacad1ef503b46029bda3d046eb18
vanarla <42469802+vanarla@users.noreply.github.com>
2018-09-04 10:05:22 +0530

    Add files via upload

--- src/com/capgemini/day7/tests/AssignmentTest.java
+++ src/com/capgemini/day7/tests/AssignmentTest.java
@@ -13,33 +13,33 @@ class AssignmentTest {
 
 		@Test
 		void testAssignment() {
-			Assignment assign1 = null;
-			Assignment assign2 = null;
-			Assignment assign3 = null;
-			Assignment assign4 = null;
+			Assignment a1 = null;
+			Assignment a2 = null;
+			Assignment a3 = null;
+			Assignment a4 = null;
 			File file = new File("C:\\Pallavi\\ExceptionHandling.txt");
 			if (file.exists() && file.isFile()) {
-				assign1 = new Assignment(file, LocalDate.of(2018, 8, 22));
+				a1 = new Assignment(file, LocalDate.of(2018, 8, 22));
 			}
 			file = new File("C:\\Pallavi\\FileHandling.txt");
 			if (file.exists() && file.isFile()) {
-				assign2 = new Assignment(file, LocalDate.of(2018, 9, 3));
+				a2 = new Assignment(file, LocalDate.of(2018, 9, 3));
 			}
 			file = new File("C:\\Pallavi\\Collection.txt");
 			if (file.exists() && file.isFile()) {
-				assign3 = new Assignment(file, LocalDate.of(2019, 8, 26));
+				a3 = new Assignment(file, LocalDate.of(2019, 8, 26));
 			}
 			file = new File("C:\\Pallavi\\Bank Account.txt");
 			if (file.exists() && file.isFile()) {
-				assign4 = new Assignment(file, LocalDate.of(2018, 8, 20));
+				a4 = new Assignment(file, LocalDate.of(2018, 8, 20));
 			}
-			assertEquals(true, Assignment.add(assign1));
-			assertEquals(true, Assignment.add(assign2));
-			assertEquals(true, Assignment.add(assign3));
-			assertEquals(true, Assignment.add(assign4));
-			assertEquals(true, Assignment.remove(assign1));
+			assertEquals(true, Assignment.add(a1));
+			assertEquals(true, Assignment.add(a2));
+			assertEquals(true, Assignment.add(a3));
+			assertEquals(true, Assignment.add(a4));
+			assertEquals(true, Assignment.remove(a1));
 			Assignment.show();
 			System.out.println("\n");
-			assertEquals(assign4, Assignment.fetchEarlyDueDate());
+			assertEquals(a4, Assignment.fetchEarlyDueDate());
 		}
 }
